8c4b12bd34b32f6260da2d31d7060d89a278f4e6
Nikita <mipo4t@gmail.com>
2019-04-07 17:06:26 +0400

    add create channel request

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,10 @@ import { createAction } from 'redux-actions';
 import routes from '../routes';
 
 export const initState = createAction('INIT_STATE');
-export const addNewMessage = createAction('ADD_NEW_MESSAGE');
 export const setErrorAlert = createAction('SET_ERROR_ALERT');
 export const deleteErrorAlert = createAction('DELETE_ERROR_ALERT');
+export const toggleMenuState = createAction('TOGGLE_MENU_STATE');
+export const addNewMessage = createAction('ADD_NEW_MESSAGE');
 
 export const sendMessage = (message, channelId) => async (dispatch) => {
   try {
@@ -14,10 +15,21 @@ export const sendMessage = (message, channelId) => async (dispatch) => {
     dispatch(addNewMessage({ message: response.data.data }));
   } catch (e) {
     dispatch(setErrorAlert({ error: 'Network error' }));
-    setTimeout(() => dispatch(deleteErrorAlert()), 5000);
     throw e;
   }
 };
 
 export const setCurrentChannel = createAction('SET_CURRENT_CHANNEL');
-export const toggleMenuState = createAction('TOGGLE_MENU_STATE');
+export const addNewChannel = createAction('ADD_NEW_CHANNEL');
+
+export const createChannel = (name, socketId) => async (dispatch) => {
+  try {
+    const url = routes.channelsUrl();
+    await axios.post(url, { data: { attributes: { name }, socketId } });
+    // const response = await axios.post(url, { data: { attributes: { name } } });
+    // dispatch(addNewChannel({ channel: response.data.data }));
+  } catch (e) {
+    dispatch(setErrorAlert({ error: 'Network error' }));
+    throw e;
+  }
+};
--- src/components/AddChannelForm.jsx
+++ src/components/AddChannelForm.jsx
@@ -3,25 +3,40 @@ import { Field, reduxForm } from 'redux-form';
 import { Form, InputGroup, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import connect from '../connect';
+import { configContextConsumerDecorator } from '../context';
 
+@connect()
 @reduxForm({ form: 'сhannelToAdd' })
+@configContextConsumerDecorator()
 class ChannelsList extends React.Component {
+  handleSubmit = async ({ channelName }) => {
+    const { reset, createChannel, socketId } = this.props;
+    await createChannel(channelName, socketId);
+    reset();
+  }
+
   render() {
+    const { handleSubmit, submitting } = this.props;
     return (
-      <Form className="mt-3">
+      <Form className="mt-3" onSubmit={handleSubmit(this.handleSubmit)}>
         <Form.Group>
           <InputGroup>
             <Field
               className="form-control"
-              name="channel"
+              name="channelName"
               component="input"
               type="text"
               placeholder="add channel"
               required
+              disabled={submitting}
             />
             <InputGroup.Append>
-              <Button variant="outline-secondary" type="submit">
-                <FontAwesomeIcon icon={faPlus} />
+              <Button variant="outline-secondary" type="submit" disabled={submitting}>
+                {submitting
+                  ? <span className="spinner-border spinner-border-sm mr-1" role="status" />
+                  : <FontAwesomeIcon icon={faPlus} />
+                }
               </Button>
             </InputGroup.Append>
           </InputGroup>
